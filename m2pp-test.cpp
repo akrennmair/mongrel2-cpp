3c5e06903a87c597d9691b87238cedcbbe64886a
Andreas Krennmair <ak@synflood.at>
2010-07-14 09:36:12 +0200

    removed dead code.

--- m2pp-test.cpp
+++ m2pp-test.cpp
@@ -32,37 +32,3 @@ int main(void) {
 	return 0;
 }
 
-
-#if 0
-
-int main(void) {
-	std::string identity("82209006-86FF-4982-B5EA-D1E29E55D481");
-	zmq::context_t ctx(2);
-
-	zmq::socket_t reqs(ctx, ZMQ_UPSTREAM);
-	reqs.connect("tcp://127.0.0.1:9999");
-
-	zmq::socket_t resp(ctx, ZMQ_PUB);
-	resp.connect("tcp://127.0.0.1:9998");
-	resp.setsockopt(ZMQ_IDENTITY, identity.data(), identity.length());
-
-	while (1) {
-		zmq::message_t inmsg;
-		reqs.recv(&inmsg);
-
-		std::string result(static_cast<const char *>(inmsg.data()), inmsg.size());
-
-		std::cout << "got " << result << std::endl;
-
-		// TODO: parse "sender conn_id path reset"
-
-		std::string reply("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 8\r\n\r\nOH HAI!\n");
-
-		zmq::message_t outmsg(reply.length());
-		::memcpy(outmsg.data(), reply.data(), reply.length());
-		resp.send(outmsg);
-	}
-
-	return 0;
-}
-#endif
